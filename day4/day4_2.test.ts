e64a6ba3e9af588083f90cd4bae2b4641b45e749
David Smith <dsmith@prometheusgroup.com>
2024-12-10 16:51:34 -0500

    day 4 part 2

--- day4/day4_2.test.ts
+++ day4/day4_2.test.ts
@@ -4,5 +4,5 @@ import { setDay, withTestInput } from '../common';
 setDay(4);
 
 test('Test answer for Day 4 part 2', async () => {
-  expect(await withTestInput(solve)).toBe(0 /*TODO: Fill me in!*/);
+  expect(await withTestInput(solve)).toBe(9);
 });
--- day4/day4_2.ts
+++ day4/day4_2.ts
@@ -1,6 +1,37 @@
-const solve = (/*TODO: add input args*/): number => {
-  //TODO: implement ME!
-  return 0;
+import { inBounds, Coord } from '../common';
+
+const solve = (input: string): number => {
+  const puzzle = input.split('\r\n').map((line) => line.split(''));
+  let result = 0;
+  for (let row = 0; row < puzzle.length; ++row) {
+    for (let col = 0; col < puzzle[row].length; ++col) {
+      const diags = diagonals([row, col]);
+      if (
+        puzzle[row][col] !== 'A' ||
+        diags.some((diag) => !inBounds(diag, puzzle))
+      )
+        continue;
+      const diaganolLetters = diags.map(([row, col]) => puzzle[row][col]);
+      if (
+        diaganolLetters.filter((letter) => letter === 'M').length === 2 &&
+        diaganolLetters.filter((letter) => letter === 'S').length === 2 &&
+        diaganolLetters[0] !== diaganolLetters[-1] &&
+        diaganolLetters[1] !== diaganolLetters[2]
+      ) {
+        result += 1;
+      }
+    }
+  }
+  return result;
+};
+
+const diagonals = ([row, col]: Coord): Coord[] => {
+  return [
+    [row + 1, col + 1],
+    [row + 1, col - 1],
+    [row - 1, col + 1],
+    [row - 1, col - 1],
+  ];
 };
 
 export { solve };
