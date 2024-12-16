1b6542d446cca3522d231655672781222f2750ac
David Smith <dsmith@prometheusgroup.com>
2024-12-16 13:00:21 -0500

    day 8 part 1

--- day8/day8_1.test.ts
+++ day8/day8_1.test.ts
@@ -4,5 +4,5 @@ import { setDay, withTestInput } from '../common';
 setDay(8);
 
 test('Test answer for Day 8 part 1', async () => {
-  expect(await withTestInput(solve)).toBe(0 /*TODO: Fill me in!*/);
+  expect(await withTestInput(solve)).toBe(14);
 });
--- day8/day8_1.ts
+++ day8/day8_1.ts
@@ -5,11 +5,26 @@ interface Antenna {
   frequency: string;
 }
 
+type Path = [number, number];
+
 const solve = (input: string): number => {
   const grid = asGrid(input);
   const antennas = getAntennas(grid);
-  console.log(pathToAntenna([0, 0], antennas[0]));
-  return 0;
+  let result = 0;
+  let outputGrid = '';
+  for (let row = 0; row < grid.length; ++row) {
+    for (let col = 0; col < grid[row].length; ++col) {
+      if (twoAntennasInLine([row, col], antennas)) {
+        result += 1;
+        outputGrid += '#';
+      } else {
+        outputGrid += grid[row][col];
+      }
+    }
+    outputGrid += '\n';
+  }
+  console.log(outputGrid);
+  return result;
 };
 
 const getAntennas = (grid: Grid): Antenna[] => {
@@ -24,14 +39,46 @@ const getAntennas = (grid: Grid): Antenna[] => {
   return result;
 };
 
-const pathToAntenna = (coord: Coord, antenna: Antenna): [number, number] => {
-  return [
-    Math.abs(coord[0] - antenna.coord[0]),
-    Math.abs(coord[1] - antenna.coord[1]),
-  ];
+const withPath = (coord: Coord, antenna: Antenna): [Antenna, Path] => {
+  return [antenna, [antenna.coord[0] - coord[0], antenna.coord[1] - coord[1]]];
 };
 
-const hasTwoAntennasInLine = (coord: Coord, antennas: Antenna[]): boolean => {};
+const AllAntennasWithPath = (
+  coord: Coord,
+  antennas: Antenna[],
+): [Antenna, Path][] => {
+  return antennas.map((antenna) => withPath(coord, antenna));
+};
+
+const twiceAsFar = (a: Path, b: Path): boolean => {
+  return a[0] * 2 === b[0] && a[1] * 2 === b[1];
+};
+
+const zeroDistance = (a: Path, b: Path): boolean => {
+  return a[0] === b[0] && a[1] === b[1];
+};
+
+const twoAntennasInLine = (
+  coord: Coord,
+  antennas: Antenna[],
+): [Antenna, Antenna] | false => {
+  const paths = AllAntennasWithPath(coord, antennas);
+
+  for (const [antenna1, path1] of paths) {
+    for (const [antenna2, path2] of paths) {
+      if (
+        !zeroDistance(path1, path2) &&
+        twiceAsFar(path1, path2) &&
+        sameFrequency(antenna1, antenna2)
+      )
+        return [antenna1, antenna2];
+    }
+  }
+  return false;
+};
+
+const sameFrequency = (a: Antenna, b: Antenna): boolean =>
+  a.frequency === b.frequency;
 
 const distance = (a: Coord, b: Coord): number =>
   Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
